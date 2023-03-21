7d11708ffa02258669e4d3108fd231729a3a30ef
Narender Majoka <narendermjk.33@gmail.com>
2023-03-21 06:07:11 +0530

    Branch Reset

--- controllers/error.js
+++
@@ -1,8 +0,0 @@
-exports.get404 = (req,res,next)=>{
-    res
-    .status(404)
-    .render('404',{pageTitle: 'Page Not Found Title', path: '/404',
-});
-    // .sendFile(path.join(__dirname,'views','page-not-found.html'));
-};
-  
